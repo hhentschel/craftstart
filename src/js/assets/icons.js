b02baad4b364967882e04c72590fe40ec4d7bc19
Henrik Hentschel <henrik.hentschel@zeix.com>
2024-06-04 17:00:07 +0200

    add new test icons

--- src/js/assets/icons.js
+++ src/js/assets/icons.js
@@ -4,11 +4,15 @@ import { dom, library } from '@fortawesome/fontawesome-svg-core';
 import {
   faPaperPlane as fasPaperPlane,
   faHouse as fasHouse,
+  faStar as fasStar,
+  faUser as fasUser,
 } from '@fortawesome/free-solid-svg-icons'
 
 library.add(
   fasPaperPlane,
-  fasHouse
+  fasHouse,
+  fasStar,
+  fasUser
 )
 
 // convert i tags to SVG
