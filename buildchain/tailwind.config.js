a517dd982dc8c3a5a7e70e9cf1bb8fe888496f18
Henrik Hentschel <henrik.hentschel@zeix.com>
2024-06-19 11:31:06 +0200

    add font sizes

--- buildchain/tailwind.config.js
+++ buildchain/tailwind.config.js
@@ -7,6 +7,23 @@ module.exports = {
   ],
   safelist: [],
   theme: {
+    fontSize: {
+      'h1-sm': ['2.375rem', {
+          lineHeight: '1.4',
+          letterSpacing: '0.06em',
+          fontWeight: '700',
+      }],
+      'h1-md': ['3rem', {
+          lineHeight: '1.4',
+          letterSpacing: '0.06em',
+          fontWeight: '700',
+      }],
+      'h1-lg': ['3.5rem', {
+          lineHeight: '1.4',
+          letterSpacing: '0.06em',
+          fontWeight: '700',
+      }],
+    },
     container: {
       center: true,
       padding: '2rem',
